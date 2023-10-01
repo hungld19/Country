e1b608ed36f055480b2a0cc40324b540ebdd6472
hungld19 <90102905+hungld19@users.noreply.github.com>
2023-10-01 11:27:55 +0700

    Add files via upload

---
+++ Country/src/view/Main.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package view;
+
+import controller.ManageEastAsiaCountries;
+import java.util.ArrayList;
+import model.Country;
+
+/**
+ *
+ * @author LAPTOP DELL
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        ArrayList<Country> lc = new ArrayList<>();
+        //loop until user want to exist
+        while (true) {
+            int choice = ManageEastAsiaCountries.menu();
+            switch (choice) {
+                case 1:
+                    ManageEastAsiaCountries.inputCountry(lc);
+                    break;
+                case 2:
+                    ManageEastAsiaCountries.printCountry(lc);
+                    break;
+                case 3:
+                    ManageEastAsiaCountries.searchByName(lc);
+                    break;
+                case 4:
+                    ManageEastAsiaCountries.printCountrySorted(lc);
+                    break;
+                case 5:
+                    return;
+            }
+        }
+    }
+}
